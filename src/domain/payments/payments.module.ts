208355a7104597b15ed2d10f28fc75bd427ca38e
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-11 10:46:28 +0700

    FEAT(payment): create payment entity & add migration

---
+++ src/domain/payments/payments.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Payment])],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
